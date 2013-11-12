430aef0219c73aac3827124e527fcc6536690608
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-12 12:13:18 -0800

    product instance methods

--- lib/products.rb
+++ lib/products.rb
@@ -31,5 +31,26 @@ attr_reader :id, :name, :vendor_id
     end
   end
 
+  def vendor
+    Vendor.all.find do |vendor|
+      vendor.id.to_i == @id.to_i
+    end
+  end
+
+  def sales 
+    Sale.all.find do |sale|
+      sale.id.to_i == @id.to_i
+    end
+  end
+
+  def number_of_sales
+    sales.amount
+  end
+
+# - `vendor` - returns the `Vendor` instance that is associated with this product using the `Product` `vendor_id` field
+# - `sales` - returns a collection of `Sale` instances that are associated with product using the `Sale` `product_id` field.
+# - `number_of_sales` - returns the number of times this product has been sold.
+
+
 end
 
