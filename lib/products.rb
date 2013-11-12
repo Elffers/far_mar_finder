f36a48f74dca7b4c39a920c78edfb485b66361d3
Anne Kimsey <anne.kimsey@gmail.com>
2013-11-12 11:03:38 -0800

    Added Product.all

--- lib/products.rb
+++ lib/products.rb
@@ -1,9 +1,18 @@
 class Product
+attr_reader :id, :name, :vendor_id
+
+def initialize(array)
+  @id = array[0]
+  @name = array [1]
+  @vendor_id = array[2]
+end
 
  def self.all
     CSV.read("./support/products.csv").map do |array|
-      Market.new()
+      Market.new(array)
     end
   end
+
+ 
 
 end
