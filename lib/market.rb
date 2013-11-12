a9d87c41fd22763d585388863dadf9afe771e37d
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-12 09:49:40 -0800

    market vendor classes

--- lib/market.rb
+++ lib/market.rb
@@ -1,14 +1,58 @@
 class Market
-  attr_reader :name, :address, :city, :county, :state, :zip
+  attr_reader :id, :name, :address, :city, :county, :state, :zip
 
   def initialize(array)
-    @name = array[1]
+    @id       = array[0]
+    @name     = array[1]
+    @address  = array[2]
+    @city     = array[3]
+    @county   = array[4]
+    @state    = array[5]
+    @zip      = array[6]
   end
 
+#`self.all` - returns all rows of the CSV file as objects
   def self.all
     CSV.read("./support/markets.csv").map do |array|
-      Market.new()
+      Market.new(array)
     end
   end
 
-end
+#returns the row where the ID field matches the argument
+  def self.find(id)
+    all.find do |market|
+      market.id.to_i == id.to_i
+    end
+  end
+
+#where X is an attribute, returns a single instance whose X attribute 
+#case-insensitive attribute matches the match parameter. For instance, 
+#Vendor.find_by_name("windler inc") could find a Vendor with the name attribute "windler inc" or "Windler Inc".
+  def self.find_by_state(match)
+    # param = attribute.downcase
+    all.find do |market|
+      market.state == match.to_s
+    end
+  end
+
+  # def self.find_by(attribute, match)
+  #   param = attribute.to_sym
+  #   all.find do |market|
+  #     market.param == match.to_s
+  #   end
+  # end
+
+#works just like `find_by_x` but returns a collection containing all possible matches. 
+#For example `Market.find_by_state("WA")` could return all of the Market object with `"WA"` in their state field.
+  def self.find_all_by_state(match)
+    all.find_all do |market|
+      market.state == match.to_s
+    end
+  end
+
+#returns a collection of `Vendor` instances that are associated with the market by the market_id field.
+  def vendors
+
+  end
+
+end # end class Market
--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,8 +1,50 @@
 class Vendor
+  attr_reader :id, :name, :num_employees, :market_id
 
-   def self.all
+  def initialize(array)
+    @id             = array[0]
+    @name           = array[1]
+    @num_employees  = array[2]
+    @market_id      = array[3]
+  end
+
+  def self.all
     CSV.read("./support/vendors.csv").map do |array|
-      Market.new()
+      Vendor.new(array)
     end
   end
+
+  def self.find(id)
+    all.find do |vendor|
+      vendor.id.to_i == id.to_i
+    end
+  end
+
+#where X is an attribute, returns a single instance whose X attribute 
+#case-insensitive attribute matches the match parameter. For instance, 
+#Vendor.find_by_name("windler inc") could find a Vendor with the name attribute "windler inc" or "Windler Inc".
+  def self.find_by_name(match)
+    all.find do |vendor|
+      vendor.name == match.to_s
+    end
+  end
+  
+  def self.find_all_by_num_employees(match)
+    all.find_all do |vendor|
+      vendor.num_employees == match.to_s
+    end
+  end
+
+
+# **Additional Market Methods**
+    
+# - `vendors` - returns a collection of `Vendor` instances that are associated with the market by the market_id field.
+
+# **Additional Vendor Methods**
+
+# - `market` - returns the `Market` instance that is associated with this vendor using the `Vendor` `market_id` field
+# - `products` - returns a collection of `Product` instances that are associated with market by the `Product` `vendor_id` field.
+# - `sales` - returns a collection of `Sale` instances that are associated with market by the `vendor_id` field.
+# - `revenue` - returns the the sum of all of the vendor's sales (in cents)
+
 end
