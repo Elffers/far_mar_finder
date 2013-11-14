bdfcfa1631df3d413e7988cc68fc0a5587c904ab
Anne Kimsey <anne.kimsey@gmail.com>
2013-11-14 13:24:34 -0800

    Documentation on vendor methods (incomplete)

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -8,25 +8,24 @@ class Vendor
     @market_id      = array[3].to_i
   end
 
+  # returns all rows of the CSV file as objects of an array 
   def self.all
     @answer ||= CSV.read("./support/vendors.csv").map do |array|
       Vendor.new(array)
     end
   end
 
+  # returns the row of the CSV where the ID field matches the argument
   def self.find(id)
     all.find do |vendor|
       vendor.id.to_i == id.to_i
     end
   end
 
-#where X is an attribute, returns a single instance whose X attribute 
-#case-insensitive attribute matches the match parameter. For instance, 
-#Vendor.find_by_name("windler inc") could find a Vendor with the name attribute "windler inc" or "Windler Inc".
 
-  def self.find_by_name(match)
+  def self.find_by_name(name)
     all.find do |vendor|
-      vendor.name == match.to_s
+      vendor.name == name.to_s
     end
   end
 
@@ -37,30 +36,34 @@ class Vendor
   end
   
   
-  def self.by_no_of_employees(match)
+  def self.by_no_of_employees(number)
     all.find_all do |vendor|
-      vendor.no_of_employees == match.to_i
+      vendor.no_of_employees == number.to_i
     end
   end
 
+  # returns the `Market` instance that is associated with a vendor instance using the `Vendor` `market_id` field
   def market
     Market.all.find do |market|
       market.id.to_i == @market_id.to_i
     end
   end
   
+  # returns a collection of `Product` instances that are associated with a vendor instance by the `Product` `vendor_id` field.
   def products
     Product.all.find_all do |product|
       product.vendor_id.to_i == @id.to_i
     end
   end
 
+  # returns a collection of `Sale` instances that are associated with a vendor instance by the `vendor_id` field.
   def sales 
     Sale.all.find_all do |sale|
       sale.vendor_id.to_i == @id.to_i
     end
   end
   
+  # returns the the sum of all a vendor instance's sales (in cents)
   def revenue
     sum = 0
     sales.each do |sale|
@@ -109,3 +112,19 @@ class Vendor
 
 end #end of Vendor class
 
+=begin
+
+
+- `self.find_by_x(match)` - where X is an attribute, returns a single instance whose X attribute case-insensitive attribute matches the match parameter. For instance, Vendor.find_by_name("windler inc") could find a Vendor with the name attribute "windler inc" or "Windler Inc".
+- `self.find_all_by_x(match)` - works just like `find_by_x` but returns a collection containing all possible matches. For example `Market.find_by_state("WA")` could return all of the Market object with `"WA"` in their state field.
+
+
+- `
+- `revenue` - 
+- `self.by_market(market_it)` - returns all of the vendors with the given `market_id`
+- `prefered_vendor` - returns the vendor with the highest revenue
+- `prefered_vendor(date)` - returns the vendor with the highest revenue for the given date
+- `worst_vendor` - returns the vendor with the lowest revenue
+- `worst_vendor(date)` - returns the vendor with the lowest revenue by date
+  
+end
