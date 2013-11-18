33818f75522be20f8dfa8bb9f1dd0ce0f0dd8001
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-18 14:07:36 -0800

    added Market.search spec

--- spec/lib/market_spec.rb
+++ spec/lib/market_spec.rb
@@ -30,6 +30,10 @@ describe Market do
       market_class.find_all_by_state("Oregon").first.name.should eq "People's Co-op Farmers Market"
     end
 
+    it "'search' should return" do
+      market_class.search("Otsiningo Park").first.zip.should eq "13905"
+    end
+
   end
   
   describe "attributes" do
